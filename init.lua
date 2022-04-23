57f9f6afbba4e9a53a64fe420195f31958807aaa
vushan-naxa <bhushan.naxa@gmail.com>
2022-04-23 10:35:37 +0545

    nvim update

--- init.lua
+++ init.lua
@@ -24,24 +24,27 @@ require "user.autocommands"
 -- set autowriteall
 -- ]])
 vim.g['auto_save'] = 1
-vim.api.nvim_set_keymap(
-  'n',
-  '<C-a>',
-  'ggVG',
-  { noremap = true }
-)
-vim.api.nvim_set_keymap(
-  'n',
-  '<C-S-Left>',
-  ':bprev <CR>',
-  { noremap = true }
-)
-vim.api.nvim_set_keymap(
-  'n',
-  '<C-S-Right>',
-  ':bnext <CR>',
-  { noremap = true }
-)
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+keymap("x", "<A-Up>", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<A-Down>", ":move '>+1<CR>gv-gv", opts)
+keymap('n', '<C-a>', 'ggVG', opts)
+keymap('n', '<C-S-Left>', ':bprev <CR>', opts)
+keymap('n','<C-S-Right>', ':bnext <CR>', opts)
+keymap('n', '<C-c>', 'Vy', opts)
+keymap('v', '<C-c>', 'y', opts)
+keymap('n', '<C-v>', '"0p', opts)
+keymap('n', '<C-z>', 'u', opts)
+keymap('n', '<C-LeftMouse>', 'gd', opts)
 
 vim.g.copilot_no_tab_map = true
 vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+local set = vim.opt
+
+set.number=true
+set.relativenumber=true
